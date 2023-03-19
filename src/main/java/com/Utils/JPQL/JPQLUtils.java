6f2a3cb2e2d5351bfcabecf6d42f77d236b6c970
sadistic001 <mouad.sadik@ig2i.centralelille.fr>
2023-03-19 21:57:09 +0100

    feature/ms-JPQL : coureurs par categorie

--- src/main/java/com/Utils/JPQL/JPQLUtils.java
+++ src/main/java/com/Utils/JPQL/JPQLUtils.java
@@ -1,8 +1,15 @@
 package com.Utils.JPQL;
 
 import com.Entities.E_Competition;
+import com.Entities.E_Coureur;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
+
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
 
 public class JPQLUtils {
 
@@ -15,5 +22,14 @@ public class JPQLUtils {
         query = em.createNamedQuery("E_Competition.getCompetitionTotalDistance");
         query.setParameter("idCompet", 1);
         return (double) query.getSingleResult();
+    }
+
+    public static List<E_Coureur> getAllCoureurInACategory(int idCategory, EntityManager em){
+        Query query = em.createQuery("SELECT c FROM E_Coureur c, E_CategorieFfa cat " +
+                "WHERE cat.id = :idCategory AND cat.ageMax >= ( FUNCTION('YEAR', CURRENT_DATE) - " +
+                "FUNCTION('YEAR', c.dateNaissance))" +
+                "AND cat.ageMin <= ( FUNCTION('YEAR', CURRENT_DATE) - FUNCTION('YEAR', c.dateNaissance))");
+       query.setParameter("idCategory", idCategory);
+       return query.getResultList();
     }
 }
--- src/main/java/com/test/JPQL/Main.java
+++ src/main/java/com/test/JPQL/Main.java
@@ -1,6 +1,7 @@
 package com.test.JPQL;
 
 import com.Entities.E_Competition;
+import com.Entities.E_Coureur;
 import com.Utils.JPQL.JPQLUtils;
 import jakarta.persistence.*;
 
@@ -11,6 +12,13 @@ public class Main {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("default");
         EntityManager em = factory.createEntityManager();
         System.out.println("distance maximale de la course 1 : " + JPQLUtils.competionTotatleDistance(1, em));
+
+        System.out.println("On affiche maintenant tous les joeurs appartenants à une catégorie");
+        List<E_Coureur> coureurs = JPQLUtils.getAllCoureurInACategory(1, em);
+        System.out.println("le nombre des coureurs : "+ coureurs.size());
+        for (E_Coureur coureur: coureurs) {
+            System.out.println(coureur);
+        }
         em.close();
 
 
